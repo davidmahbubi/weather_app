7202fca9e5febacb64e3f2eace2cfc823524a0a6
David Mahbubi <ulrichdavid0370@gmail.com>
2020-08-20 22:08:39 +0700

    Added bootstrap to project dependencies, not devDependencies

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import Vue from 'vue';
-import {
-  BootstrapVue,
-  IconsPlugin
-} from 'bootstrap-vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.vue';
